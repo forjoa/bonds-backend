a2fac0dbe35bad923f4a1c92b668ceef94509afd
Rebeca\Rebe y Joa <joaquin_trujillo@icloud.com>
2024-08-07 15:03:19 +0200

    build: implement db connection

---
+++ config/database.js
@@ -0,0 +1,9 @@
+import { createClient } from '@libsql/client';
+import { config } from 'dotenv';
+
+config()
+
+export const db = createClient({
+    url: process.env.TURSO_DATABASE_URL || 'libsql://talk-forjoa.turso.io', // update this line
+    authToken: process.env.TURSO_AUTH_TOKEN,
+});

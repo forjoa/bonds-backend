866444974d8d6cdc080a049116c3501fc1679566
Rebeca\Rebe y Joa <joaquin_trujillo@icloud.com>
2024-08-11 15:43:56 +0200

    refactor: delete useless function

--- src/config/database.js
+++ src/config/database.js
@@ -16,10 +16,3 @@ export const sql = postgres({
         options: `project=${ENDPOINT_ID}`,
     },
 });
-
-async function getPgVersion() {
-    const result = await sql`select version()`;
-    console.log(result);
-}
-
-getPgVersion();

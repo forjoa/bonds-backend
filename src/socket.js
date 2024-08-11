89125a63f36e2e99a27f61cc1d81d6b10bd8986b
Rebeca\Rebe y Joa <joaquin_trujillo@icloud.com>
2024-08-11 15:17:37 +0200

    refactor: implement new handlers

--- src/socket.js
+++ src/socket.js
@@ -1,19 +1,32 @@
-// TODO: change all this file 
-export default function socketHandlers(io) {
-    io.on('connection', (socket) => {
-        console.log('a user is connected');
+import { validateToken } from './socket/auth.js';
+import handleChatEvents from './socket/chatHandler.js';
+import handleLikeEvents from './socket/likeHandler.js';
+import handleCommentEvents from './socket/commentHandler.js';
+import handleFriendRequestEvents from './socket/friendRequestHandler.js';
 
-        socket.on('joinRoom', (room) => {
-            socket.join(room);
-            console.log(`User joined room ${room}`);
-        });
+export default function socketHandlers(io) {
+    io.use((socket, next) => {
+        const token = socket.handshake.auth.token;
+        const user = validateToken(token);
+        if (user) {
+            socket.userId = user.id;  // Asigna el ID del usuario al socket
+            socket.join(`user_${user.id}`);  // Une al usuario a un room específico
+            next();
+        } else {
+            next(new Error('Authentication error'));
+        }
+    });
+
+    io.on('connection', (socket) => {
+        console.log(`User ${socket.userId} connected`);
+
+        handleChatEvents(io, socket);
+        handleLikeEvents(io, socket);
+        handleCommentEvents(io, socket);
+        handleFriendRequestEvents(io, socket);
 
         socket.on('disconnect', () => {
-            console.log('user disconnected');
-        });
-
-        socket.on('chat message', ({ room, msg }) => {
-            io.to(room).emit('chat message', msg);
+            console.log(`User ${socket.userId} disconnected`);
         });
     });
 }

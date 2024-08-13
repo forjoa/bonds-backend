ff290f6af6202f4efd07df27bb65bea449064b3d
Rebeca\Rebe y Joa <joaquin_trujillo@icloud.com>
2024-08-13 23:36:47 +0200

    build: auth service to verify a token

---
+++ src/services/authService.js
@@ -0,0 +1,26 @@
+import jwt from 'jsonwebtoken';
+import { config } from 'dotenv';
+import { sql } from '../config/database.js';
+
+config();
+
+export const verifyTokenService = async ({ token }) => {
+    try {
+        const decoded = jwt.verify(token, process.env.SIGNATURE);
+
+        const userId = decoded.userid;
+
+        const result = await sql`SELECT * FROM users WHERE userid = ${userId}`;
+
+        if (!result[0]) {
+            return { success: false, message: 'User not found.' };
+        }
+
+        const userInfo = result[0];
+        const { password: _, ...userPayload } = userInfo;
+
+        return { success: true, user: userPayload };
+    } catch (error) {
+        return { success: false, message: 'Invalid or expired token.' };
+    }
+};

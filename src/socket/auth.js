f47119b5b48bd19bcbb94ea64d5faf4fdab7a593
Rebeca\Rebe y Joa <joaquin_trujillo@icloud.com>
2024-08-14 18:24:57 +0200

    fix: update signature

--- src/socket/auth.js
+++ src/socket/auth.js
@@ -1,8 +1,11 @@
 import jwt from 'jsonwebtoken';
+import { config } from 'dotenv';
+
+config();
 
 export function validateToken(token) {
     try {
-        const decoded = jwt.verify(token, 'secret');
+        const decoded = jwt.verify(token, process.env.SIGNATURE);
         return decoded;
     } catch (err) {
         return null;

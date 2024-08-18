9ab35fe448ef8f9b2b4c89f7fea14078177bae14
Rebeca\Rebe y Joa <joaquin_trujillo@icloud.com>
2024-08-18 15:41:52 +0200

    buidl: get home service

--- src/services/postService.js
+++ src/services/postService.js
@@ -5,3 +5,27 @@ export const uploadPostService = async ({ userid, content }) => {
 
     return result.length === 0 ? { success: true, message: 'Post uploaded correctly.' } : { success: false, message: 'Something went wrong.' }
 }
+
+export const getHomeService = async ({ userid }) => {
+    const result = await sql`SELECT
+                                posts.postid,
+                                posts.content,
+                                posts.createdat,
+                                users.username,
+                                users.userid
+                            FROM
+                                posts
+                                JOIN users on posts.userid = users.userid
+                                LEFT JOIN friendships on (
+                                    friendships.userid = users.userid
+                                    OR friendships.otheruserid = users.userid
+                                )
+                            WHERE
+                                posts.userid = ${userid}
+                                OR friendships.userid = ${userid}
+                                OR friendships.otheruserid = ${userid}
+                            ORDER BY posts.createdat DESC
+                            ;`
+
+    return result
+}

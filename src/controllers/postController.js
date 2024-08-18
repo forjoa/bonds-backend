e8d2ef8f0710cca7f1247fcc78e3937492cbc698
Rebeca\Rebe y Joa <joaquin_trujillo@icloud.com>
2024-08-18 15:42:01 +0200

    build: get home controller

--- src/controllers/postController.js
+++ src/controllers/postController.js
@@ -1,8 +1,17 @@
-import { uploadPostService } from "../services/postService.js"
+import { getHomeService, uploadPostService } from "../services/postService.js"
 
 export const uploadPost = async (req, res) => {
     try {
         const result = await uploadPostService(req.body)
+        res.json(result)
+    } catch (error) {
+        res.status(500).json({ error: error.message })
+    }
+}
+
+export const getHome = async (req, res) => {
+    try {
+        const result = await getHomeService(req.body)
         res.json(result)
     } catch (error) {
         res.status(500).json({ error: error.message })

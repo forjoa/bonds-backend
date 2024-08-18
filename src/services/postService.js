8c315e2da4b5b3fe77ab087bc95641a22083ec7e
Rebeca\Rebe y Joa <joaquin_trujillo@icloud.com>
2024-08-18 16:26:58 +0200

    fix: update query to make a photos array

--- src/services/postService.js
+++ src/services/postService.js
@@ -7,25 +7,33 @@ export const uploadPostService = async ({ userid, content }) => {
 }
 
 export const getHomeService = async ({ userid }) => {
-    const result = await sql`SELECT
+    const result = await sql`
+                            SELECT
                                 posts.postid,
                                 posts.content,
                                 posts.createdat,
                                 users.username,
-                                users.userid
+                                users.userid,
+                                COALESCE(
+                                    json_agg(photos.url) FILTER (WHERE photos.url IS NOT NULL), '[]'
+                                ) AS photos
                             FROM
                                 posts
                                 JOIN users on posts.userid = users.userid
                                 LEFT JOIN friendships on (
-                                    friendships.userid = users.userid
-                                    OR friendships.otheruserid = users.userid
+                                    (friendships.userid = users.userid AND friendships.otheruserid = ${userid})
+                                    OR (friendships.otheruserid = users.userid AND friendships.userid = ${userid})
                                 )
+                                LEFT JOIN photos on posts.postid = photos.postid
                             WHERE
                                 posts.userid = ${userid}
                                 OR friendships.userid = ${userid}
                                 OR friendships.otheruserid = ${userid}
-                            ORDER BY posts.createdat DESC
-                            ;`
+                            GROUP BY
+                                posts.postid, posts.content, posts.createdat, users.username, users.userid
+                            ORDER BY
+                                posts.createdat DESC;
+                        `;
 
     return result
 }

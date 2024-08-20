9ffdb427a16ca2edabc8161fb8386c0ea06636c6
Rebeca\Rebe y Joa <joaquin_trujillo@icloud.com>
2024-08-20 20:26:31 +0200

    feat: send comment endpoint

--- src/routes/postRoute.js
+++ src/routes/postRoute.js
@@ -1,10 +1,11 @@
 import express from 'express';
-import { getHome, like, uploadPost } from '../controllers/postController.js';
+import { comment, getHome, like, uploadPost } from '../controllers/postController.js';
 
 const router = express.Router();
 
 router.post('/uploadPost', uploadPost)
 router.post('/getHome', getHome)
 router.post('/like', like)
+router.post('/comment', comment)
 
 export default router

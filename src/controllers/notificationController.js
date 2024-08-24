0db3c790e8f6c41817a23fc520657d4b83c1474d
Rebeca\Rebe y Joa <joaquin_trujillo@icloud.com>
2024-08-24 23:05:20 +0200

    build: new notification controller

---
+++ src/controllers/notificationController.js
@@ -0,0 +1,10 @@
+import { newNotificationService } from "../services/notificationService.js"
+
+export const newNotification = async (req, res) => {
+    try {
+        const result = await newNotificationService(req.body)
+        res.json(result)
+    } catch (error) {
+        res.status(500).json({ error: error.message })
+    }
+}

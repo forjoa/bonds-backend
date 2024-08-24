0c9fa76e6c647aa340432786841fdedcc88d69cc
Rebeca\Rebe y Joa <joaquin_trujillo@icloud.com>
2024-08-24 23:04:59 +0200

    build: insert new notifications

---
+++ src/services/notificationService.js
@@ -0,0 +1,16 @@
+import { sql } from "../config/database.js"
+
+export const newNotificationService = async ({ userid, type, referenceid, seen = false }) => {
+    await sql`INSERT INTO notifications (
+                                    userid, 
+                                    type, 
+                                    referenceid, 
+                                    seen
+                                    ) 
+                            VALUES (
+                                    ${userid}, 
+                                    ${type}, 
+                                    ${referenceid}, 
+                                    ${seen}
+                        )`
+}

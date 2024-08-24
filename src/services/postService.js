15c27e3919c376ff15785022c556e0f1d09b10c6
Rebeca\Rebe y Joa <joaquin_trujillo@icloud.com>
2024-08-24 16:59:32 +0200

    build: query to get my posts

--- src/services/postService.js
+++ src/services/postService.js
@@ -100,3 +100,55 @@ export const commentService = async ({ userid, postid, content }) => {
 
     return result.length === 0 ? { success: true, message: 'Comment sent correctly.' } : { success: false, message: 'Something went wrong.' }
 }
+
+export const getMyPostsService = async ({ userid, page, limit }) => {
+    const offset = (page - 1) * limit;
+
+    const result = await sql`
+        SELECT
+            posts.postid,
+            posts.content,
+            posts.createdat,
+            users.username,
+            users.userid,
+            users.fullname,
+            users.profilephoto,
+            COALESCE(
+                json_agg(DISTINCT photos.url) FILTER (WHERE photos.url IS NOT NULL), '[]'
+            ) AS photos,
+            COUNT(DISTINCT likes.likeid) AS likecount,
+            EXISTS (
+                SELECT 1 FROM likes
+                WHERE likes.postid = posts.postid
+                AND likes.userid = ${userid}
+            ) AS userliked,
+            COALESCE(
+                json_agg(
+                    DISTINCT jsonb_build_object(
+                        'commentid', comments.commentid,
+                        'content', comments.content,
+                        'createdat', comments.createdat,
+                        'userid', comment_users.userid,
+                        'fullname', comment_users.fullname,
+                        'username', comment_users.username
+                    )
+                ) FILTER (WHERE comments.commentid IS NOT NULL), '[]'
+            ) AS comments
+        FROM
+            posts
+            JOIN users ON posts.userid = users.userid
+            LEFT JOIN photos ON posts.postid = photos.postid
+            LEFT JOIN likes ON posts.postid = likes.postid
+            LEFT JOIN comments ON posts.postid = comments.postid
+            LEFT JOIN users AS comment_users ON comments.userid = comment_users.userid
+        WHERE
+            posts.userid = ${userid}
+        GROUP BY
+            posts.postid, posts.content, posts.createdat, users.username, users.userid, users.fullname, users.profilephoto
+        ORDER BY
+            posts.createdat DESC
+        LIMIT ${limit} OFFSET ${offset};
+    `;
+
+    return result;
+};

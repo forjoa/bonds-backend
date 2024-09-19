9347b2ee9996cb2730536fbae7de5a5b2d0051b2
Rebeca\Rebe y Joa <joaquin_trujillo@icloud.com>
2024-09-19 21:59:05 +0200

    fix: encryption typing

--- src/lib/encryption.ts
+++ src/lib/encryption.ts
@@ -1,10 +1,11 @@
 import { SignJWT, jwtVerify } from 'jose'
 import { hash, compare } from 'bcrypt'
+import { JWTPayload } from 'ts-jose'
 
 const secretKey = 'secret'
 const key = new TextEncoder().encode(secretKey)
 
-export const encrypt = async (payload) => {
+export const encrypt = async (payload: JWTPayload) => {
   return await new SignJWT(payload)
     .setProtectedHeader({ alg: 'HS256' })
     .setIssuedAt()
@@ -12,17 +13,17 @@ export const encrypt = async (payload) => {
     .sign(key)
 }
 
-export const decrypt = async (input) => {
+export const decrypt = async (input: string) => {
   const { payload } = await jwtVerify(input, key, {
     algorithms: ['HS256'],
   })
   return payload
 }
 
-export const hashPasswords = async (password) => {
+export const hashPasswords = async (password: string) => {
   return await hash(password, 10)
 }
 
-export const comparePasswords = async (password, hashedPassword) => {
+export const comparePasswords = async (password: string, hashedPassword: string) => {
   return await compare(password, hashedPassword)
 }
